e602c72870f7fede0b41709d1d1a5fd24fa1dc4d
m-triple-m <triplem656@gmail.com>
2025-01-01 22:25:10 +0530

    boring

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
+
+  int a = 10; int b = 20;
+
+  print(a-b);
 }
 
 class MyApp extends StatelessWidget {
   MyApp({super.key});
+
+  String name = "Mubassir";
+  int age = 10;
+  double pi = 3.14275365;
+  bool isHungry = true;
+
+
 
   @override
   Widget build(BuildContext context) {
